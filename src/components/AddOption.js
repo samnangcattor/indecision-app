ca518d242a49cda85731b02fbff9ee88e139d43b
Dam SamNang <dam.sam.nang@framgia.com>
2018-06-20 16:11:02 +0700

    source maps with webpack

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -12,7 +12,7 @@ export default class AddOption extends React.Component {
 
   handleAddOption(e) {
     e.preventDefault();
-
+    console.log('testing');
     const option = e.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option);
 
