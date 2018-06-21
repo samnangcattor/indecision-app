4635fc7ab470b3f62967babc08bac24ee9037f13
Dam SamNang <dam.sam.nang@framgia.com>
2018-06-21 12:58:33 +0700

    add style big button

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,7 +3,12 @@ import Option from './Option';
 
 const Options = (props) => (
   <div>
-    <button onClick={props.handleDeleteOptions}>Remove all</button>
+    <button
+      className="button button--link"
+      onClick={props.handleDeleteOptions}
+    >
+        Remove all
+    </button>
     {props.options.length === 0 && <p>Please add item</p>}
     {
       props.options.map((option) => (
