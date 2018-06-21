a9d7b447633cee81925d0d38026bc17dc6f88568
Dam SamNang <dam.sam.nang@framgia.com>
2018-06-21 11:19:23 +0700

    using variables with scss

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Header = (Props) => (
   <div className="header">
-    <h1 className="header__title">{Props.title}</h1>
-    {Props.subtitle && <h2 className="header__subtitle">{Props.subtitle}</h2>}
+    <div className="container">
+      <h1 className="header__title">{Props.title}</h1>
+      {Props.subtitle && <h2 className="header__subtitle">{Props.subtitle}</h2>}
+    </div>
   </div>
 );
 
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -77,15 +77,17 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle} />
-        <Action hasOptions={this.state.hasOptions()} handlePick={this.handlePick} />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption
-          handleAddOption={this.handleAddOption}
-        />
+        <div className="container">
+          <Action hasOptions={this.state.hasOptions()} handlePick={this.handlePick} />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption
+            handleAddOption={this.handleAddOption}
+          />
+        </div>
         <OptionModal 
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
