eb6b6da519b18dc70745d9a566c6b28b88f4d646
Dam SamNang <dam.sam.nang@framgia.com>
2018-06-21 13:15:52 +0700

    add style options list

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,13 +3,16 @@ import Option from './Option';
 
 const Options = (props) => (
   <div>
-    <button
-      className="button button--link"
-      onClick={props.handleDeleteOptions}
-    >
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+      <button
+        className="button button--link"
+        onClick={props.handleDeleteOptions}
+      >
         Remove all
-    </button>
-    {props.options.length === 0 && <p>Please add item</p>}
+      </button>
+    </div>
+    {props.options.length === 0 && <p className="widget__message">Please add item</p>}
     {
       props.options.map((option) => (
         <Option
